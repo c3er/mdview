9a1a210bab2a1816832ff990e5a187591e833406
Christian Dreier <flipper.x3r@googlemail.com>
2018-04-08 00:45:48 +0200

    Enable syntax highlighting

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const path = require("path")
 const fs = require("fs")
-const marked = require("marked")
+const highlight = require("highlight.js")
 
-document.getElementById('content').innerHTML =
-    marked(fs.readFileSync(path.join(__dirname, "README.md"), "utf8"))
+const markdown = require("markdown-it")({
+    highlight: (str, lang) => {
+        // Taken from VS Code
+        if (lang && highlight.getLanguage(lang)) {
+            try {
+                return `<pre class="hljs"><code><div>${highlight.highlight(lang, str, true).value}</div></code></pre>`
+            } catch (err) {
+                console.log(`Error at highlighting: ${err}`)
+            }
+        }
+        return `<pre class="hljs"><code><div>${markdown.utils.escapeHtml(str)}</div></code></pre>`
+    }
+})
+
+let content = fs.readFileSync(path.join(__dirname, "testfile.md"), "utf8")
+document.getElementById("content").innerHTML = markdown.render(content)
