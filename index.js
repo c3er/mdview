c3er/mdview

commit c9a350045b3520027e920bb711c12d9c3d370e94

Remember scroll position after refreshing

index.js
@@ -36,6 +36,8 @@ const TITLE = "Markdown Viewer"
 
 const _blockedElements = {}
 
+let _scrollPosition = 0
+
 function generateCodeText(text, options = {}) {
     const defaults = {
         isHighlighted: false,
@@ -175,6 +177,7 @@ electron.ipcRenderer.on("fileOpen", (_, filePath, internalTarget) => {
         statusOnMouseOver(image, `${image.getAttribute("alt")} (${imageUrl})`)
 
         image.onerror = () => image.style.backgroundColor = "#ffe6cc"
+        image.onload = () => window.scrollTo(0, _scrollPosition)
     })
 
     let titlePrefix = filePath
@@ -234,6 +237,13 @@ electron.ipcRenderer.on("unblockAll", unblockAll)
 
 electron.ipcRenderer.on("viewRawText", () => {
     switchRawView(true)
+})
+
+electron.ipcRenderer.on("prepareReload", () => electron.ipcRenderer.send("reloadPrepared", document.documentElement.scrollTop))
+
+electron.ipcRenderer.on("restorePosition", (_, position) => {
+    window.scrollTo(0, position)
+    _scrollPosition = position
 })
 
 window.addEventListener('keyup', event => {

main.js
@@ -16,6 +16,9 @@ let _currentFilePath
 let _contentIsBlocked = false
 const _unblockedURLs = []
 
+let _isReloading = false
+let _scrollPosition = 0
+
 function error(msg) {
     dialog.showErrorBox("Error", `${msg}. Exiting.`)
     process.exit(1)
@@ -76,6 +79,12 @@ function createWindow() {
         })
     )
     _mainWindow.on("closed", () => _mainWindow = null)
+    _mainWindow.webContents.on("did-finish-load", () => {
+        if (_isReloading) {
+            _mainWindow.webContents.send("restorePosition", _scrollPosition)
+            _isReloading = false
+        }
+    })
 
     _mainMenu = electron.Menu.buildFromTemplate([
         {
@@ -128,7 +137,7 @@ function createWindow() {
                     label: "Refresh",
                     accelerator: "F5",
                     click() {
-                        _mainWindow.reload()
+                        _mainWindow.webContents.send("prepareReload")
                     }
                 },
                 {
@@ -231,3 +240,9 @@ electron.ipcMain.on("allContentUnblocked", () => {
 electron.ipcMain.on("disableRawView", () => allowRawTextView(false))
 
 electron.ipcMain.on("enableRawView", () => allowRawTextView(true))
+
+electron.ipcMain.on("reloadPrepared", (_, position) => {
+    _scrollPosition = position
+    _isReloading = true
+    _mainWindow.reload()
+})

commit 764161a71f8e1ba1fcf3386e512f996dfa10eece

Bugfix at unblocking images
The position changed to the document's top.

This bug seems to introduced by commit c9a350045b3520027e920bb711c12d9c3d370e94

index.js
@@ -188,7 +188,6 @@ electron.ipcRenderer.on("fileOpen", (_, filePath, internalTarget, encoding) => {
         statusOnMouseOver(image, `${image.getAttribute("alt")} (${imageUrl})`)
 
         image.onerror = () => image.style.backgroundColor = "#ffe6cc"
-        image.onload = () => window.scrollTo(0, _scrollPosition)
     })
 
     let titlePrefix = filePath