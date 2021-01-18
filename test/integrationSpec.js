f35e1717be04a510ff46f51a97cf40d6911676c8
Christian Dreier <flipper.x3r@googlemail.com>
2021-01-18 23:10:19 +0100

    Add integration tests for blocked content

--- test/integrationSpec.js
+++ test/integrationSpec.js
@@ -4,13 +4,22 @@ const assert = require("chai").assert
 
 const lib = require("./lib")
 
+const elements = {
+    blockedContentArea: {
+        path: "//div[@id='blocked-content-info']",
+        closeButton: {
+            path: "//span[@id='blocked-content-info-close-button']",
+        },
+    },
+}
+
+const defaultDocumentFile = "testfile_utf8.md"
+const defaultDocumentPath = path.join(__dirname, "documents", defaultDocumentFile)
+
+let app
+let client
+
 describe("Integration tests with single app instance", () => {
-    const defaultDocumentFile = "testfile_utf8.md"
-    const defaultDocumentPath = path.join(__dirname, "documents", defaultDocumentFile)
-
-    let app
-    let client
-
     before(async () => {
         app = await lib.startApp(defaultDocumentPath)
         client = app.client
@@ -28,7 +37,41 @@ describe("Integration tests with single app instance", () => {
     })
 
     it("displays blocked content banner", async () => {
-        const elem = await client.$("//div[@id='blocked-content-info']")
+        const elem = await client.$(elements.blockedContentArea.path)
         assert.eventually.equal(elem.getAttribute("hidden"), null)
+    })
+})
+
+describe("Integration tests with their own app instance each", () => {
+    beforeEach(async () => {
+        app = await lib.startApp(defaultDocumentPath)
+        client = app.client
+    })
+
+    afterEach(async () => await lib.stopApp(app))
+
+    describe("Blocked content element", () => {
+        it("disappears at click on X", async () => {
+            const blockedContentElement = await client.$(elements.blockedContentArea.path)
+            const closeButton = await client.$(elements.blockedContentArea.closeButton.path)
+
+            closeButton.click()
+            assert.eventually.equal(blockedContentElement.getAttribute("hidden"), true)
+        })
+
+        it("unblocks content", async () => {
+            const blockedContentElement = await client.$(elements.blockedContentArea.path)
+            blockedContentElement.click()
+
+            assert.isTrue(await lib.wait(async () => {
+                let hasFoundUnblockedMessage = false;
+                (await client.getMainProcessLogs()).forEach(log => {
+                    if (log.toLowerCase().includes("unblocked")) {
+                        hasFoundUnblockedMessage = true
+                    }
+                })
+                return hasFoundUnblockedMessage
+            }))
+        })
     })
 })
--- test/lib.js
+++ test/lib.js
@@ -5,6 +5,12 @@ const chaiAsPromised = require("chai-as-promised")
 const electron = require("electron")
 const Application = require("spectron").Application
 
+// Based on https://stackoverflow.com/a/39914235/13949398 (What is the JavaScript version of sleep()?)
+function sleep(ms) {
+    // console.log(`sleep ${ms} ${process.hrtime()}`) // For debugging
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 global.before(() => chai.use(chaiAsPromised))
 
 exports.startApp = async documentPath => {
@@ -23,4 +29,16 @@ exports.stopApp = async app => {
     if (app && app.isRunning()) {
         await app.stop()
     }
+}
+
+exports.wait = async (predicate, tries, timeout) => {
+    tries = tries || 10
+    timeout = timeout || 100
+    for (let i = 0; i < tries; i++) {
+        if (await predicate()) {
+            return true
+        }
+        await sleep(timeout)
+    }
+    return false
 }
