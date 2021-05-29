c3er/mdview

commit af58b8a8ee80533735c527775c7ed4449acdca86

Add tests for settings storage

app/lib/storage.js
@@ -1,10 +1,15 @@
 const fs = require("fs")
 const path = require("path")
 
-const electron = require("electron")
+let electron = require("electron")
 
 const SETTINGS_FILE = "settings.json"
 const ENCODINGS_FILE = "encodings.json"
+
+const LIGHT_THEME = "light"
+const DARK_THEME = "dark"
+
+let _isTest = false
 
 let _settings
 let _encodings
@@ -18,8 +23,10 @@ class StorageBase {
         this._storagePath = path.join(storageDir, storageFile)
         this._data = StorageBase._initData(this._storagePath)
 
-        console.log(`Initialized storage file ${this._storagePath}`)
-        console.log(`Storage content: ${JSON.stringify(this._data)}`)
+        if (!_isTest) {
+            console.log(`Initialized storage file ${this._storagePath}`)
+            console.log(`Storage content: ${JSON.stringify(this._data)}`)
+        }
     }
 
     _save() {
@@ -44,15 +51,12 @@ class StorageBase {
 class Settings extends StorageBase {
     #THEME_KEY = "theme"
 
-    LIGHT_THEME = "light"
-    DARK_THEME = "dark"
-
     get theme() {
         return this._data[this.#THEME_KEY] ?? electron.nativeTheme.themeSource
     }
 
     set theme(value) {
-        const allowedThemes = [this.LIGHT_THEME, this.DARK_THEME]
+        const allowedThemes = [LIGHT_THEME, DARK_THEME]
         if (!allowedThemes.includes(value)) {
             throw {
                 message: `"${value}" is not in allowed values ${allowedThemes.join(", ")}`,
@@ -86,10 +90,19 @@ exports.SETTINGS_FILE = SETTINGS_FILE
 
 exports.ENCODINGS_FILE = ENCODINGS_FILE
 
+exports.LIGHT_THEME = LIGHT_THEME
+
+exports.DARK_THEME = DARK_THEME
+
 exports.getDefaultDir = getDefaultDir
 
-exports.initSettings = (settingsStorageDir, settingsStorageFile) =>
-    _settings ?? (_settingss = new Settings(settingsStorageDir, settingsStorageFile))
+exports.initSettings = (settingsStorageDir, settingsStorageFile, electronMock) => {
+    if (electronMock) {
+        electron = electronMock
+        _isTest = true
+    }
+    return _settings ?? (_settingss = new Settings(settingsStorageDir, settingsStorageFile))
+}
 
 exports.initEncodings = (encodingStorageDir, encodingStorageFile) =>
     _encodings ?? (_encodings = new Encodings(encodingStorageDir, encodingStorageFile))

test/integrationSpec.js
@@ -1,4 +1,3 @@
-const fs = require("fs")
 const path = require("path")
 
 const assert = require("chai").assert
@@ -121,6 +120,45 @@ describe("Integration tests with single app instance", () => {
     describe('Library "storage"', () => {
         const dataDir = path.join(__dirname, "data")
         const storage = require("../app/lib/storage")
+
+        describe("Settings", () => {
+            const DEFAULT_THEME = storage.LIGHT_THEME
+
+            let settings
+            let electronMock
+
+            beforeEach(() => {
+                electronMock = {
+                    nativeTheme: {
+                        themeSource: DEFAULT_THEME,
+                    },
+                }
+                settings = storage.initSettings(dataDir, storage.SETTINGS_FILE, electronMock)
+                settings.theme = DEFAULT_THEME
+            })
+
+            describe("Theme", () => {
+                it("has a default theme", () => {
+                    assert.equal(settings.theme, DEFAULT_THEME)
+                })
+
+                it("remembers light theme", () => {
+                    const theme = storage.LIGHT_THEME
+                    settings.theme = theme
+                    assert.equal(settings.theme, theme)
+                })
+
+                it("remembers dark theme", () => {
+                    const theme = storage.DARK_THEME
+                    settings.theme = theme
+                    assert.equal(settings.theme, theme)
+                })
+
+                it("does not accept an unknown theme", () => {
+                    assert.throws(() => settings.theme = "invalid-theme")
+                })
+            })
+        })
 
         describe("Encodings", () => {
             const encodings = storage.initEncodings(dataDir, storage.ENCODINGS_FILE)

commit 98ff1166c5e505410ebc8c14f478716f92a9d930

Fix regression at theme switching
Introduced by commit af58b8a8ee80533735c527775c7ed4449acdca86

app/lib/storage.js
@@ -6,9 +6,6 @@ let electron = require("electron")
 const SETTINGS_FILE = "settings.json"
 const ENCODINGS_FILE = "encodings.json"
 
-const LIGHT_THEME = "light"
-const DARK_THEME = "dark"
-
 let _isTest = false
 
 let _settings
@@ -49,12 +46,15 @@ class StorageBase {
 class Settings extends StorageBase {
     #THEME_KEY = "theme"
 
+    LIGHT_THEME = "light"
+    DARK_THEME = "dark"
+
     get theme() {
         return this._data[this.#THEME_KEY] ?? electron.nativeTheme.themeSource
     }
 
     set theme(value) {
-        const allowedThemes = [LIGHT_THEME, DARK_THEME]
+        const allowedThemes = [this.LIGHT_THEME, this.DARK_THEME]
         if (!allowedThemes.includes(value)) {
             throw {
                 message: `"${value}" is not in allowed values ${allowedThemes.join(", ")}`,
@@ -87,10 +87,6 @@ function getDefaultDir() {
 exports.SETTINGS_FILE = SETTINGS_FILE
 
 exports.ENCODINGS_FILE = ENCODINGS_FILE
-
-exports.LIGHT_THEME = LIGHT_THEME
-
-exports.DARK_THEME = DARK_THEME
 
 exports.getDefaultDir = getDefaultDir
 

test/integrationSpec.js
@@ -48,6 +48,10 @@ const elements = {
                     label: "View Raw Text",
                     isEnabled: true,
                 },
+                switchTheme: {
+                    label: "Switch Theme",
+                    isEnabled: true,
+                },
             },
         },
         encoding: {
@@ -81,14 +85,18 @@ const defaultDocumentPath = path.join(__dirname, "documents", defaultDocumentFil
 let app
 let client
 
-async function checkUnblockedMessage() {
-    let hasFoundUnblockedMessage = false
+async function containsConsoleMessage(message) {
+    let hasFoundMessage = false
     ;(await client.getMainProcessLogs()).forEach(log => {
-        if (log.toLowerCase().includes("unblocked")) {
-            hasFoundUnblockedMessage = true
+        if (log.toLowerCase().includes(message)) {
+            hasFoundMessage = true
         }
     })
-    return hasFoundUnblockedMessage
+    return hasFoundMessage
+}
+
+async function checkUnblockedMessage() {
+    return await containsConsoleMessage("unblocked")
 }
 
 async function elementIsVisible(element) {
@@ -118,12 +126,12 @@ describe("Integration tests with single app instance", () => {
     })
 
     describe('Library "storage"', () => {
+        const DEFAULT_THEME = "light"
+
         const dataDir = path.join(__dirname, "data")
         const storage = require("../app/lib/storage")
 
         describe("Settings", () => {
-            const DEFAULT_THEME = storage.LIGHT_THEME
-
             let settings
             let electronMock
 
@@ -143,13 +151,13 @@ describe("Integration tests with single app instance", () => {
                 })
 
                 it("remembers light theme", () => {
-                    const theme = storage.LIGHT_THEME
+                    const theme = settings.LIGHT_THEME
                     settings.theme = theme
                     assert.equal(settings.theme, theme)
                 })
 
                 it("remembers dark theme", () => {
-                    const theme = storage.DARK_THEME
+                    const theme = settings.DARK_THEME
                     settings.theme = theme
                     assert.equal(settings.theme, theme)
                 })
@@ -256,6 +264,14 @@ describe("Integration tests with their own app instance each", () => {
             await assert.eventually.isFalse(
                 elementIsVisible(await client.$("//div[@class='markdown-body']"))
             )
+        })
+    })
+
+    describe("Theme switching", () => {
+        it("can be done", async () => {
+            const viewMenu = elements.mainMenu.view
+            await menuAddon.clickMenu(viewMenu.label, viewMenu.sub.switchTheme.label)
+            await assert.eventually.isFalse(containsConsoleMessage("error"))
         })
     })
 })