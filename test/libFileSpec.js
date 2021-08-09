3b63206371b2baef035eafc21edc9eaf28081c61
Christian Dreier <flipper.x3r@googlemail.com>
2021-08-09 22:26:33 +0200

    Remove test/lib.js
    It was not needed outside the integration tests that are in a single
    spec file.

--- test/libFileSpec.js
+++ test/libFileSpec.js
@@ -6,7 +6,7 @@ const file = require("../app/lib/file")
 
 describe('Library "file"', () => {
     const mdFilePath = path.join(__dirname, "documents", "testfile_utf8.md")
-    const nonMdFilePath = path.join(__dirname, "lib.js")
+    const nonMdFilePath = path.join(__dirname, "mocking.js")
     const binFilePath = path.join(__dirname, "documents", "font1", "Daedric.ttf")
 
     describe("file.open", () => {
