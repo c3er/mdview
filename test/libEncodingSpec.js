f049b1309f96ea312a4e8ee799f5f2135910b2b0
Christian Dreier <flipper.x3r@googlemail.com>
2021-11-09 15:56:05 +0100

    Add tests for encoding detection

---
+++ test/libEncodingSpec.js
@@ -0,0 +1,48 @@
+const assert = require("chai").assert
+
+const ipc = require("../app/lib/ipc")
+
+const mocking = require("./mocking")
+
+describe("Encoding library", () => {
+    describe("Main part", () => {
+        const encodingLib = require("../app/lib/encoding/encodingMain")
+
+        const encoding = "utf16le"
+        const filename = "testfile"
+
+        beforeEach(() => encodingLib.init(mocking.mainMenu, mocking.dataDir, mocking.electron))
+
+        it("remembers the encoding", () => {
+            encodingLib.change(filename, encoding)
+            assert.equal(encodingLib.load(filename), encoding)
+        })
+
+        it("reacts to changeEncoding IPC message", () => {
+            const message = ipc.messages.changeEncoding
+            mocking.register.ipc.mainOn(message, (_, filePath, actualEncoding) => {
+                assert.equal(filePath, filename)
+                assert.equal(actualEncoding, encoding)
+            })
+            mocking.send.ipc.toMain(message, null, filename, encoding)
+        })
+    })
+
+    describe("Renderer part", () => {
+        const encodingLib = require("../app/lib/encoding/encodingRenderer")
+
+        const iso88592Encoding = "iso88592"
+        const iso88592Buffer = Buffer.from(
+            "\x4d\xfc\x6c\x6c\x65\x72\x20\x53\x74\x72\x61\xdf\x65",
+            "binary"
+        )
+
+        it("detects ISO-8859-2 encoded bytes", () => {
+            assert.equal(encodingLib.detect(iso88592Buffer), iso88592Encoding)
+        })
+
+        it("decodes ISO-8859-2 encoded bytes", () => {
+            assert.equal(encodingLib.decode(iso88592Buffer, iso88592Encoding), "Müller Straße")
+        })
+    })
+})
