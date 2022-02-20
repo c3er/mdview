fdade2234748cef1c128805b05b71725d9e9de3f
Christian Dreier <flipper.x3r@googlemail.com>
2022-02-20 12:32:00 +0100

    Update application initialization to current Electron pattern

--- app/main.js
+++ app/main.js
@@ -428,7 +428,7 @@ function createWindow() {
     return [mainWindow, mainMenu]
 }
 
-electron.app.on("ready", () => {
+electron.app.whenReady().then(() => {
     cli.init()
     _cliArgs = cli.parse(process.argv)
 
@@ -449,21 +449,22 @@ electron.app.on("ready", () => {
     encodingLib.init(mainMenu)
     contentBlocking.init(mainWindow, mainMenu)
     rawText.init(mainWindow, mainMenu)
+
+    electron.app.on("activate", function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (electron.BrowserWindow.getAllWindows().length === 0) {
+            ;[_mainWindow, _mainMenu] = createWindow()
+        }
+    })
 })
 
 electron.app.on("window-all-closed", () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
     if (process.platform !== "darwin") {
         electron.app.quit()
-    }
-})
-
-electron.app.on("activate", () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (_mainWindow === null) {
-        createWindow()
     }
 })
 
