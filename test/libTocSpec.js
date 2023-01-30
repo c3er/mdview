2e6537e2cd94b0ed1ede4150fd535611119be97f
Christian Dreier <flipper.x3r@googlemail.com>
2023-01-30 03:19:24 +0100

    Minor refactoring at TOC tests

--- test/libTocSpec.js
+++ test/libTocSpec.js
@@ -3,14 +3,12 @@ const assert = require("chai").assert
 const toc = require("../app/lib/renderer/toc")
 
 describe('Library "TOC"', () => {
-    it("Recognizes a header", () => {
+    it("recognizes a header", () => {
         const headerText = "Some header"
         const content = `# ${headerText}`
         assert.isTrue(
             toc.build(content).equals(
                 toc.Section.fromObject({
-                    header: "",
-                    parent: null,
                     subSections: [
                         {
                             header: headerText,
@@ -22,7 +20,7 @@ describe('Library "TOC"', () => {
         )
     })
 
-    it("Recognizes two headers", () => {
+    it("recognizes two headers", () => {
         const headerText1 = "Header 1"
         const headerText2 = "Header 2"
         const content = `
@@ -33,8 +31,6 @@ describe('Library "TOC"', () => {
         assert.isTrue(
             toc.build(content).equals(
                 toc.Section.fromObject({
-                    header: "",
-                    parent: null,
                     subSections: [
                         {
                             header: headerText1,
