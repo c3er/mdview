557006156d802d4d187e0d2337825263ff7733ee
Christian Dreier <flipper.x3r@googlemail.com>
2023-06-25 19:41:58 +0200

    Improve callback handling
    The callback is only handled, if the user confirmed the dialog and the
    input field contains some text.

--- app/lib/renderer/search.js
+++ app/lib/renderer/search.js
@@ -1,3 +1,5 @@
+const CANCEL_VALUE = "search-dialog-cancel"
+
 let _document
 let _searchDialog
 
@@ -5,7 +7,12 @@ exports.init = (document, callback) => {
     _document = document
 
     _searchDialog = _document.getElementById("search-dialog")
-    _searchDialog.addEventListener("close", () => callback(_searchDialog.returnValue))
+    _searchDialog.addEventListener("close", () => {
+        const result = _searchDialog.returnValue
+        if (result && result !== CANCEL_VALUE) {
+            callback(result)
+        }
+    })
 
     const okButton = _document.getElementById("search-ok-button")
     const searchInputElement = _document.getElementById("search-input")
