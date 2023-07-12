a9d2513755f99ddfeb6c39a543780f95c55670cf
Christian Dreier <flipper.x3r@googlemail.com>
2023-07-13 01:16:23 +0200

    Remove superfluous files (macOS)

--- scripts/afterPack.js
+++ scripts/afterPack.js
@@ -6,36 +6,78 @@ async function removeFiles(dir, files) {
         try {
             const filePath = path.join(dir, file)
             console.log(`Removing "${filePath}"...`)
-            await fs.rm(filePath)
+            await fs.rm(filePath, { recursive: true })
         } catch (error) {
             console.log(error)
         }
     }
 }
 
+async function removeResources(dir, filesToKeep) {
+    console.log(`Removing resources from "${dir}"...`)
+    console.log(`Files to keep: ${filesToKeep.join(", ")}`)
+    await removeFiles(
+        dir,
+        (await fs.readdir(dir)).filter(file => !filesToKeep.includes(file)),
+    )
+}
+
 exports.default = async context => {
     const LOCALES_DIR = "locales"
     const LOCALE_TO_KEEP = "en-US.pak"
+
+    const MAC_RESOURCE_DIR =
+        "mdview.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources"
+    const MAC_RESOURCES_TO_KEEP = [
+        "Info.plist",
+        "MainMenu.nib",
+        "chrome_100_percent.pak",
+        "chrome_200_percent.pak",
+        "en.lproj",
+        "resources.pak",
+        "v8_context_snapshot.arm64.bin",
+        "icudtl.dat",
+    ]
+
     const FILES_TO_REMOVE = {
         win32: [
-            "d3dcompiler_47.dll",
-            "vk_swiftshader.dll",
-            "vk_swiftshader_icd.json",
-            "vulkan-1.dll",
+            {
+                dir: ".",
+                files: [
+                    "d3dcompiler_47.dll",
+                    "vk_swiftshader.dll",
+                    "vk_swiftshader_icd.json",
+                    "vulkan-1.dll",
+                ],
+            },
         ],
         linux: [], // XXX Propr Linux box needed
+        darwin: [
+            {
+                dir: "mdview.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Libraries",
+                files: ["libvk_swiftshader.dylib", "vk_swiftshader_icd.json"],
+            },
+        ],
     }
 
     const outDir = context.appOutDir
+    const platform = context.electronPlatformName
 
     const outDirContent = await fs.readdir(outDir)
     if (outDirContent.includes(LOCALES_DIR)) {
-        const localesDir = path.join(outDir, LOCALES_DIR)
-        await removeFiles(
-            localesDir,
-            (await fs.readdir(localesDir)).filter(locale => locale !== LOCALE_TO_KEEP),
-        )
+        await removeResources(path.join(outDir, LOCALES_DIR), [LOCALE_TO_KEEP])
     }
 
-    await removeFiles(outDir, FILES_TO_REMOVE[context.electronPlatformName])
+    const fileInfos = FILES_TO_REMOVE[platform]
+    if (fileInfos === undefined) {
+        console.log(`Warning: platform "${platform}" not supported`)
+        return
+    }
+    for (const info of fileInfos) {
+        await removeFiles(path.join(outDir, info.dir), info.files)
+    }
+
+    if (platform === "darwin") {
+        await removeResources(path.join(outDir, MAC_RESOURCE_DIR), MAC_RESOURCES_TO_KEEP)
+    }
 }
