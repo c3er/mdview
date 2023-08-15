d06929acfc11427cc41e7c77bf8620fb7d7221cb
Christian Dreier <flipper.x3r@googlemail.com>
2023-08-15 15:28:19 +0200

    Add click handlers for settings dialog tabs

--- app/lib/settings/settingsRenderer.js
+++ app/lib/settings/settingsRenderer.js
@@ -1,18 +1,35 @@
 const ipc = require("../ipc/ipcRenderer")
 
+const DIALOG_TAB_CLASS = "dialog-tab"
+const DIALOG_TAB_CONSTENT_CLASS = "dialog-tab-content"
+
 let _document
 let _dialogElement
 
 let _dialogIsOpen = false
 
+let _tabElements
+let _tabContentElements
+
+function changeTab(tabIndex) {
+    for (const [i, tabContentElement] of _tabContentElements.entries()) {
+        tabContentElement.style.display = i === tabIndex ? "block" : "none"
+    }
+}
+
 exports.init = document => {
     _document = document
+
+    _tabElements = [..._document.getElementsByClassName(DIALOG_TAB_CLASS)]
+    _tabContentElements = [..._document.getElementsByClassName(DIALOG_TAB_CONSTENT_CLASS)]
+    for (const [i, tabElement] of _tabElements.entries()) {
+        tabElement.addEventListener("click", () => changeTab(i))
+    }
+    changeTab(0)
 
     _dialogElement = _document.getElementById("settings-dialog")
     _dialogElement.addEventListener("close", () => {
         _dialogIsOpen = false
-
-        // ...
         ipc.send(ipc.messages.settingsDialogIsOpen, false)
     })
 
