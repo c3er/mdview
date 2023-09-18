8ffddd3bb610cc290ad173fe4252bc7908de9d0c
Christian Dreier <flipper.x3r@googlemail.com>
2023-09-18 20:45:29 +0200

    Put file history tests into a "storage description"

--- test/libStorage.spec.js
+++ test/libStorage.spec.js
@@ -5,53 +5,55 @@ const mocking = require("./mocking")
 
 const storage = require("../app/lib/main/storage")
 
-describe("File history", () => {
-    let applicationSettings
-    let fileHistory
+describe("Storage", () => {
+    describe("File history", () => {
+        let applicationSettings
+        let fileHistory
 
-    function addFiles(files) {
-        for (const file of files) {
-            fileHistory.add(file)
+        function addFiles(files) {
+            for (const file of files) {
+                fileHistory.add(file)
+            }
+            assert.isTrue(fileHistory.hasFiles())
         }
-        assert.isTrue(fileHistory.hasFiles())
-    }
 
-    beforeEach(async () => {
-        await lib.removeDataDir()
-        storage.reset()
+        beforeEach(async () => {
+            await lib.removeDataDir()
+            storage.reset()
 
-        storage.init(mocking.dataDir, mocking.electron)
+            storage.init(mocking.dataDir, mocking.electron)
 
-        applicationSettings = storage.loadApplicationSettings()
-        fileHistory = storage.loadFileHistory()
-    })
+            applicationSettings = storage.loadApplicationSettings()
+            fileHistory = storage.loadFileHistory()
+        })
 
-    it("is empty by default", () => {
-        assert.strictEqual(fileHistory.filePaths.length, 0)
-        assert.isFalse(fileHistory.hasFiles())
-    })
+        it("is empty by default", () => {
+            assert.strictEqual(fileHistory.filePaths.length, 0)
+            assert.isFalse(fileHistory.hasFiles())
+        })
 
-    it("remembers files", () => {
-        const files = ["File 1", "File 2", "File 3"]
-        addFiles(files)
-        assert.deepEqual(fileHistory.filePaths, files.reverse())
-    })
+        it("remembers files", () => {
+            const files = ["File 1", "File 2", "File 3"]
+            addFiles(files)
+            assert.deepEqual(fileHistory.filePaths, files.reverse())
+        })
 
-    it("has a maximum number of files", () => {
-        const files = ["File 1", "File 2", "File 3", "File 4", "File 5"]
-        const historySize = (applicationSettings.fileHistorySize = 3)
+        it("has a maximum number of files", () => {
+            const files = ["File 1", "File 2", "File 3", "File 4", "File 5"]
+            const historySize = (applicationSettings.fileHistorySize = 3)
 
-        addFiles(files)
+            addFiles(files)
 
-        const fileCount = files.length
-        assert.deepEqual(
-            fileHistory.filePaths,
-            files.reverse().slice(0, fileCount - (fileCount - historySize)),
-        )
-    })
+            const fileCount = files.length
+            assert.deepEqual(
+                fileHistory.filePaths,
+                files.reverse().slice(0, fileCount - (fileCount - historySize)),
+            )
+        })
 
-    it("has only unique entries", () => {
-        addFiles(["File 1", "File 2", "File 1"])
-        assert.deepEqual(fileHistory.filePaths, ["File 1", "File 2"])
+        it("has only unique entries", () => {
+            addFiles(["File 1", "File 2", "File 1"])
+            assert.deepEqual(fileHistory.filePaths, ["File 1", "File 2"])
+        })
     })
 })
