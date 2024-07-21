8d132475055a795dbf03202facadbef44328e16b
Christian Dreier <flipper.x3r@googlemail.com>
2024-07-21 15:50:39 +0200

    Improve naming of build artifacts
    While the application is named "Markdown Viewer" and should be
    displayed as such at the appropriate places in the OS, the exe file
    should be named "mdview".

--- build/deployConfig.js
+++ build/deployConfig.js
@@ -1,11 +1,14 @@
 const APPLICATION_NAME = "Markdown Viewer"
+const APPLICATION_SHORTNAME = "mdview"
 const WIN_ICON = "app/assets/icon/md.ico"
 const MAC_ICON = "app/assets/icon/md.icns"
 
 module.exports = {
+    productName: APPLICATION_NAME,
+    executableName: APPLICATION_SHORTNAME,
+    artifactName: "${name}-${version}-${arch}.${ext}",
     afterPack: "build/afterPack.js",
     afterAllArtifactBuild: "build/afterAllArtifactBuild.js",
-    artifactName: "${name}-${version}-${arch}.${ext}",
     files: [
         "!**/node_modules/**/{test,__tests__,tests,powered-test,example,examples}",
         "!**/*/{.eslintrc.json,.eslintrc.js,.eslintignore,.nvmrc,tsconfig.json,.stylelintrc.json,.prettierignore,.prettierrc,.prettierrc.json,.babelrc,babel.config.js,bower.json,webpack.config.js,.browserslist,rollup.config.js,.editorconfig}",
