c3er/mdview

commit 10a69a5ae454d775b5c2418ddedebd1a1848bc40

Changes in structure of library
Sub directories "renderer" and "main" don't exist anymore. The
containing files now have respective suffixes instead, like all the
other modules.

It seems easier and should take less maintenance in the praxis.

app/lib/cliMain.js
@@ -1,10 +1,10 @@
 const path = require("path")
 
-const log = require("../log")
+const log = require("./log")
 
 let electron
 
-const DEFAULT_FILE = path.join(__dirname, "..", "..", "..", "README.md")
+const DEFAULT_FILE = path.join(__dirname, "..", "..", "README.md")
 
 function extractInternalTarget(args) {
     return args.find(arg => arg.startsWith("#"))

app/index.js
@@ -9,8 +9,8 @@ const remote = require("@electron/remote")
 const about = require("./lib/aboutRenderer")
 const common = require("./lib/common")
 const contentBlocking = require("./lib/contentBlockingRenderer")
-const dialog = require("./lib/renderer/dialog")
-const documentRendering = require("./lib/renderer/documentRendering")
+const dialog = require("./lib/dialogRenderer")
+const documentRendering = require("./lib/documentRenderingRenderer")
 const dragDrop = require("./lib/dragDropRenderer")
 const encodingLib = require("./lib/encodingRenderer")
 const error = require("./lib/errorRenderer")
@@ -19,7 +19,7 @@ const ipc = require("./lib/ipcRenderer")
 const log = require("./lib/log")
 const navigation = require("./lib/navigationRenderer")
 const rawText = require("./lib/rawTextRenderer")
-const renderer = require("./lib/renderer/common")
+const renderer = require("./lib/commonRenderer")
 const search = require("./lib/searchRenderer")
 const settings = require("./lib/settingsRenderer")
 const toc = require("./lib/tocRenderer")

app/lib/aboutMain.js
@@ -2,8 +2,8 @@ const path = require("path")
 
 const common = require("./common")
 const ipc = require("./ipcMain")
-const menu = require("./main/menu")
-const packageJson = require("./main/packageJson")
+const menu = require("./menuMain")
+const packageJson = require("./packageJsonMain")
 
 const ABOUT_DIALOG_MENU_ID = "about"
 

app/lib/aboutRenderer.js
@@ -1,4 +1,4 @@
-const dialog = require("./renderer/dialog")
+const dialog = require("./dialogRenderer")
 const ipc = require("./ipcRenderer")
 
 let electron

app/lib/contentBlockingMain.js
@@ -1,7 +1,7 @@
 const common = require("./common")
 const ipc = require("./ipcMain")
 const log = require("./log")
-const menu = require("./main/menu")
+const menu = require("./menuMain")
 const navigation = require("./navigationMain")
 
 const UNBLOCK_CONTENT_MENU_ID = "unblock-content"

app/lib/documentRenderingRenderer.js
@@ -1,7 +1,7 @@
 const hljs = require("highlight.js")
 
-const metadata = require("./metadata")
-const toc = require("../tocRenderer.js")
+const metadata = require("./metadataRenderer")
+const toc = require("./tocRenderer")
 
 let _markdown
 
@@ -77,7 +77,7 @@ exports.reset = options => {
         .use(require("markdown-it-task-checkbox"), { disabled: false })
 
     if (options.emojisEnabled) {
-        _markdown.use(require("../../../node_modules/markdown-it-emoji/dist/markdown-it-emoji.js"))
+        _markdown.use(require("markdown-it-emoji/dist/markdown-it-emoji.js"))
     }
 
     _shallRenderAsMarkdown = options.renderAsMarkdown

commit c675f7f5930c1f27614839096b08103378d006fa

Fix a regression caused by recent refactoring
Introduced in commit 10a69a5ae454d775b5c2418ddedebd1a1848bc40

During development, the settings files were stored a directory level too
high.

app/lib/cliMain.js
@@ -38,7 +38,7 @@ function parseTestArgs(args) {
         storageDirArgIndex = testArgIndex + 1
     }
     const storageDir = isDevelopment()
-        ? path.join(__dirname, "..", "..", "..", ".storage")
+        ? path.join(__dirname, "..", "..", ".storage")
         : path.join(electron.app.getPath("userData"), "storage")
 
     return {