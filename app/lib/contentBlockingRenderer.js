3ff68b50bee44b4b7e14a465b8fdad9f297ee7aa
Christian Dreier <flipper.x3r@googlemail.com>
2025-06-22 22:52:07 +0200

    Fill content blocking dialog with blocked URLs

--- app/lib/contentBlockingRenderer.js
+++ app/lib/contentBlockingRenderer.js
@@ -15,7 +15,46 @@ let _window
 let _unblockContentButton
 let _dialogElement
 
-let _blockedElements = {}
+let _contents = {}
+
+class Content {
+    url = ""
+    elements = []
+    isBlocked = true
+
+    constructor(url) {
+        this.url = url
+        this.elements = searchElementsWithAttributeValue(url)
+        for (const element of this.elements) {
+            element.onclick = () => createUnblockMenu(url)
+        }
+    }
+
+    unblock() {
+        for (const element of this.elements) {
+            element.removeAttribute("style")
+
+            // Force element to reload without recreating the DOM element.
+            // Recreating the DOM element would cause the attached event handlers to be lost.
+            const attributes = element.attributes
+            for (let i = 0; i < attributes.length; i++) {
+                const attr = attributes[i]
+                const value = attr.nodeValue
+                if (value === this.url) {
+                    element.setAttribute(attr.nodeName, value)
+                }
+            }
+        }
+    }
+
+    toHtml() {
+        return `
+            <tr>
+                <td>${this.url}</td>
+            </tr>
+        `
+    }
+}
 
 function searchElementsWithAttributeValue(value) {
     // Based on https://stackoverflow.com/a/30840550 (JQuery selector using value, but unknown attribute)
@@ -85,27 +124,14 @@ function createUnblockMenu(url) {
 }
 
 function hasBlockedElements() {
-    return !common.isEmptyObject(_blockedElements)
+    return !common.isEmptyObject(_contents)
 }
 
 function unblockUrl(url) {
     ipc.send(ipc.messages.unblockUrl, url)
 
-    for (const element of _blockedElements[url] ?? []) {
-        element.removeAttribute("style")
-
-        // Force element to reload without recreating the DOM element.
-        // Recreating the DOM element would cause the attached event handlers to be lost.
-        const attributes = element.attributes
-        for (let i = 0; i < attributes.length; i++) {
-            const attr = attributes[i]
-            const value = attr.nodeValue
-            if (value === url) {
-                element.setAttribute(attr.nodeName, value)
-            }
-        }
-    }
-    delete _blockedElements[url]
+    _contents[url]?.unblock()
+    delete _contents[url]
 
     if (!hasBlockedElements()) {
         changeInfoElementVisiblity(false)
@@ -116,7 +142,7 @@ function unblockUrl(url) {
 }
 
 function unblockAll() {
-    for (const url in _blockedElements) {
+    for (const url in _contents) {
         unblockUrl(url)
     }
 }
@@ -126,16 +152,30 @@ function storeUnblockedUrl(url) {
     log.info(`Stored unblocked URL: ${url}`)
 }
 
+function storeAllUnblocked() {
+    for (const url in _contents) {
+        storeUnblockedUrl(url)
+    }
+}
+
 function openDialog() {
     dialog.open(
         DIALOG_ID,
-        () => _dialogElement.showModal(),
+        () => {
+            _document.querySelector("dialog #content-blocking-dialog-scroll-container").innerHTML =
+                `
+                    <table>
+                        ${Object.values(_contents).map(content => content.toHtml())}
+                    </table>
+                `
+            _dialogElement.showModal()
+        },
         () => _dialogElement.close(),
     )
 }
 
 function reset() {
-    _blockedElements = {}
+    _contents = {}
 }
 
 exports.init = (document, window, shallForceInitialization, remoteMock) => {
@@ -151,19 +191,20 @@ exports.init = (document, window, shallForceInitialization, remoteMock) => {
     _dialogElement = _document.querySelector("dialog#content-blocking-dialog")
 
     renderer.addStdButtonHandler(_unblockContentButton, createUnblockAllMenu)
-    renderer.addStdButtonHandler(_document.querySelector("button#content-blocking-ok-button"), () =>
-        _dialogElement.close(),
+    renderer.addStdButtonHandler(
+        _document.querySelector("button#content-blocking-ok-button"),
+        () => {
+            storeAllUnblocked()
+            unblockAll()
+            _dialogElement.close()
+        },
     )
     renderer.addStdButtonHandler(
         _document.querySelector("button#content-blocking-cancel-button"),
         () => _dialogElement.close(),
     )
     ipc.listen(ipc.messages.contentBlocked, url => {
-        const elements = (_blockedElements[url] = searchElementsWithAttributeValue(url))
-        for (const element of elements) {
-            element.onclick = () => createUnblockMenu(url)
-        }
-
+        _contents[url] = new Content(url)
         changeInfoElementVisiblity(true)
         _document.querySelector("span#blocked-content-info-close-button").onclick = () =>
             changeInfoElementVisiblity(false)
